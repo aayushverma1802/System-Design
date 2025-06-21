b73f23bc1ee09ced35c81a5801dc968720bdd0f9
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2025-06-21 11:41:21 +0530

    Add files via upload

---
+++ Modular Programming/app.cpp
@@ -0,0 +1,10 @@
+#include"Operators.h"
+#include<iostream>
+
+int main(){
+    //Numbers used 6 and 9
+    std::cout<<"ADD "<<add(6,9)<<std::endl;
+    std::cout<<"SUB "<<sub(6,9)<<std::endl;
+    std::cout<<"MUL "<<mul(6,9)<<std::endl;
+    std::cout<<"DIV "<<div(6.0f,9.0f)<<std::endl;
+}
