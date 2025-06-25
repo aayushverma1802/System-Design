81090d33d8c2761496667acfcfd1029d3bbf781a
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2025-06-25 08:56:04 +0530

    Add files via upload

---
+++ Override/Virtual_Function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+    virtual void func()
+    {
+        cout << "This is the base class\n";
+    }
+};
+class Derived : public Base
+{
+public:
+    void func()
+    {
+        cout << "This is the Derived class\n";
+    }
+};
+int main()
+{
+    Base *ptr;
+    Derived obj;
+    ptr=&obj;
+    ptr->func();
+}
